458a1c454755004ca817a5144eef2963ce563084
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2022-03-04 14:55:34 +0300

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,45 @@ impl StatusMessage {
         self.records.get(&account_id).cloned()
     }
 }
+
+// use the attribute below for unit tests
+#[cfg(test)]
+mod tests {
+    use crate::StatusMessage;
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
+    // part of writing unit tests is setting up a mock context
+    // in this example, this is only needed for env::log in the contract
+    // this is also a useful list to peek at when wondering what's available in env::*
+    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
+        VMContext {
+            current_account_id: "alice.testnet".to_string(),
+            signer_account_id: "robert.testnet".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id: "jane.testnet".to_string(),
+            input,
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage: 0,
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view,
+            output_data_receivers: vec![],
+            epoch_height: 19,
+        }
+    }
+    #[test]
+    fn set_get_message() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let mut contract = StatusMessage::default();
+        contract.set_status("hello".to_string());
+        assert_eq!(
+            "hello".to_string(),
+            contract.get_status("robert.testnet".to_string()).unwrap()
+        );
+    }
+}
